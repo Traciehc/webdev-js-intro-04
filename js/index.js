b075d8cc9b6efe8c220daf5bd99a709230161a2c
Tracie Childers <traciechilders01@gmail.com>
2024-12-23 20:39:36 -0500

    Did experiment in index.js and commented out code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let healthPoints = 7;
+
+if (healthPoints <= 7) {
+        console.log("Hero is not healthy.");
+} else if (healthPoints < 7 && healthpoints >= 5) {
+        console.log("Hero needs more energy!");
+} else if (healthPoints <= 3);{
+        console.log("Hero is nearly dead!");
+} 
+
+Output:  Hero is not healthy.  Hero is nearly dead!
+        
+let healthPoints = 20;
+
+if (healthPoints >= 10 && healthPoints <= 15) {
+    console.log("Hero needs energy!");
+} else {
+    console.log("Hero is healthy.")
+} 
+
+Output:  Hero is healthy.
+
+
+let healthPoints = 10;
+
+if (healthPoints < 5) {
+    console.log("Hero needs energy!")
+}else {
+    console.log("Hero is healthy.")
+}
+Output:  Hero is healthy.
+
+let healthPoints = 15;
+
+if (healthPoints <= 10 && healthPoints >= 5) {
+    console.log("Hero needs more energy!");
+}else if (healthPoints < 5) {
+    console.log("Hero is nearly dead!");
+}
+Output:  Hero needs more energy!  */
